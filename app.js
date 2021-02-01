cc1f62dbb5273cb8d1fba702f9b3f29fa4667f03
testuiacc <78386285+testuiacc@users.noreply.github.com>
2021-02-01 23:06:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ searchBar.addEventListener('keyup', function(e){
     const term = e.target.value.toLowerCase();
     const posts = list.getElementsByTagName('li');
     Array.from(posts).forEach(function(post){
-        console.log('ZZZZZZZ')
         const title = post.firstElementChild.textContent;
         if(title.toLowerCase().indexOf(term) != -1){
             post.style.display = 'block';
-            console.log('dsadsa');
         }
         else {
             post.style.display = 'none';
-            console.log("XDXDXD")
         }
     })
 })
